324f38825033f8f4c38eb7002424395a190e14f1
Rajat13rg <rajatrg2005@gmail.com>
2026-06-26 22:10:51 +0530

    b

--- config/db.js
+++ config/db.js
@@ -4,4 +4,5 @@ require("dotenv").config();
 const connectDB = async () => {
         await mongoose.connect(process.env.mongo_url).then(()=>console.log("DB connected"));
 }
+
 module.exports = connectDB;
